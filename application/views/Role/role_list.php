e754e88cb42839a4623173625d98dc9910abe9f1
Punam Hon <punamhon5@gmail.com>
2025-11-14 18:07:25 +0530

    role_list.php file is updated.

--- application/views/Role/role_list.php
+++ application/views/Role/role_list.php
@@ -1,35 +1,47 @@
-<div class="container">
-    <div class="card">
-        <div class="card-header">
-            <a href="<?= base_url('role/add') ?>" class="btn btn-primary float-right">Add Role</a>
-        </div>
-        <div class="card-body">
-            <table  id="" class="table table-bordered table-striped" style="vertical-align:middle;">
-                <thead>
-                    <tr>
-                        <th>SR NO</th>
-                        <th>Role ID</th>
-                        <th>User Role</th>
-                        <th>Role Status</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php if($roles): ?>
-                        <?php foreach ($roles as $count =>$role):?>
-                            <tr>
-                                <th><?= ++$count ?></th>
-                                <th><?= $role->role_id ?></th>
-                                <th><?= $role->usr_role ?></th>
-                                <th><?= $role->role_st ?></th>
-                                <td class="text_center"><a href="<?= base_url('Role/view/'.$role->role_id )?>"> <i class="fa fa-eye"></i></a></td>
-                                <td class="text-center"><a href="<?= base_url('Role/edit/'.$role->role_id); ?>"> <i class="fa fa-edit"></i></a></td>
-                                <td class="text-center"><a href="<?= base_url('Role/delete'.$role->role_id); ?>"> <i class="fa fa-trash text-danger"></i></a></td>
-                            </tr>
-                        <?php endforeach ?>
-                    <?php endif; ?>
-                </tbody>
+<table class="table table-bordered table-striped">
+      <a href="<?= base_url('Role/add'); ?>" class="btn btn-primary float-right">Add Staff</a>
+       <!-- Flash Messages -->
+            <?php include('assets/incld/messages.php') ?>
 
-            </table>
-        </div>
-    </div>
-</div>
+    <thead>
+        <tr>
+            <th>SR NO</th>
+            <th>Role ID</th>
+            <th>User Role</th>
+            <th>Status</th>
+            <th colspan="3" class="text-center">Action</th>
+        </tr>
+    </thead>
+
+    <tbody>
+        <?php if (!empty($roles)): ?>
+            <?php foreach ($roles as $i => $role): ?>
+                <tr>
+                    <td><?= $i + 1 ?></td>
+                    <td><?= $role->role_id ?></td>
+                    <td><?= $role->usr_role ?></td>
+                    <td><?= $role->role_st ?></td>
+
+                    <td class="text-center">
+                        <a href="<?= base_url('Role/view/'.$role->role_id) ?>">
+                            <i class="fa fa-eye"></i>
+                        </a>
+                    </td>
+
+                    <td class="text-center">
+                        <a href="<?= base_url('Role/edit/'.$role->role_id) ?>">
+                            <i class="fa fa-edit"></i>
+                        </a>
+                    </td>
+
+                    <td class="text-center">
+                        <a href="<?= base_url('Role/delete/'.$role->role_id) ?>" 
+                           onclick="return confirm('Are you sure?')">
+                            <i class="fa fa-trash text-danger"></i>
+                        </a>
+                    </td>
+                </tr>
+            <?php endforeach; ?>
+        <?php endif; ?>
+    </tbody>
+</table>
