2c9daddbc5207c558fe00557e5c610bbc2e28ae4
Bhupesh Kumbhar <bhupesh.saptech@gmail.com>
2025-11-16 17:02:41 +0530

    dashboard removed

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -61,7 +61,7 @@ class User extends CI_Controller
         $this->load->view('incld/header');
 		$this->load->view('incld/top_menu');
 		$this->load->view('incld/side_menu');
-		$this->load->view('admin/dashboard');
+	//	$this->load->view('admin/dashboard');
 		$this->load->view('user/list',$data);
 		$this->load->view('incld/jslib');
 		$this->load->view('incld/footer');
