4c29c5f9e750b6029c98288e6d82a00735d06b3f
Vaishnavithakare14 <129979612+Vaishnavithakare14@users.noreply.github.com>
2025-11-17 16:44:00 +0530

    staff added

--- application/controllers/Staff.php
+++ application/controllers/Staff.php
@@ -15,7 +15,7 @@ class Staff extends CI_Controller {
         $this->load->view('incld/header');
         $this->load->view('incld/top_menu');
         $this->load->view('incld/side_menu');
-        $this->load->view('admin/dashboard');
+        $this->load->view('user/dashboard');
         $this->load->view('staff/staff_list', $data);
         $this->load->view('incld/jslib');
         $this->load->view('incld/footer');
